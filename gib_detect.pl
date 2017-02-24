2ec3f96f2fede51781f2bd8123bee0b852f427e5
Ben Humphrey <bennyh6813@gmail.com>
2017-02-24 18:42:54 -0500

    formatting change

--- gib_detect.pl
+++ gib_detect.pl
@@ -12,7 +12,7 @@ while (1) {
     my $l         = <STDIN>;
     my $model_mat = $model_data->{mat};
     my $threshold = $model_data->{thresh};
-	my $debugOutput = avg_transition_prob($l, $model_mat);
+    my $debugOutput = avg_transition_prob($l, $model_mat);
     if (avg_transition_prob($l, $model_mat) > $threshold) {
 	    print "debug $debugOutput";
         print "=> true\n";
