885407acf003269a48686e0eb4978ed0f4c092ca
Ben Humphrey <bennyh6813@gmail.com>
2017-02-24 22:59:17 -0500

    add exception word list and string input

--- gib_detect.pl
+++ gib_detect.pl
@@ -6,19 +6,71 @@ use warnings;
 use Storable qw(retrieve);
 require('gib_detect_train.pl');
 
-my $model_data = retrieve('gib_model.dat');
+my $model_file     = 'gib_model.dat';
+my $exception_file = 'exception.txt';
+
+my $model_data = retrieve($model_file);
+
+# Read exceptions (one per line)
+my %exceptions;
+{
+    open my $fh, '<', $exception_file
+      or die "Can't open file $exception_file: $!";
+    while (defined(my $line = <$fh>)) {
+        $line = join(' ', split(' ', lc($line)));    # normalize
+        $exceptions{$line} = 1;
+    }
+    close $fh;
+}
 
 while (1) {
-    my $l         = <STDIN>;
-    my $model_mat = $model_data->{mat};
-    my $threshold = $model_data->{thresh};
-    my $debugOutput = avg_transition_prob($l, $model_mat);
-    if (avg_transition_prob($l, $model_mat) > $threshold) {
-	    print "debug $debugOutput";
-        print "=> true\n";
+    print "Input: ";
+    my $line = <STDIN>;
+
+    if (not defined $line) {
+        last;
+    }
+
+    # Slits the line into words
+    my @words = split(' ', lc($line));
+
+    my %result;
+    foreach my $word (@words) {
+
+        # Word exists in the exceptions list
+        if (exists $exceptions{$word}) {
+            $result{$word} += 1;
+            next;
+        }
+
+        my $model_mat = $model_data->{mat};
+        my $threshold = $model_data->{thresh};
+
+        my $prob = avg_transition_prob($word, $model_mat);
+
+        if ($prob > $threshold) {
+            $result{$word} += 1;
+        }
+        else {
+            $result{$word} -= 1;
+        }
+    }
+
+    if (not @words) {
+        next;
+    }
+
+    my $sum  = sum(values %result);
+    my $diff = $sum - @words;
+
+    if ($diff == 0) {
+        print "Line does not contain gibberish words.\n";
     }
     else {
-	    print "debug $debugOutput";
-        print "=> false\n";
+        my @gibberish = grep { $result{$_} < 0 } @words;
+        my $count = scalar @gibberish;
+        print "Line contains $count gibberish words.\n";
+        print "The gibberish words are: ";
+        print join(', ', @gibberish), "\n";
     }
 }
